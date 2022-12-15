d89464242fb6d17c38a41155b7080bd795b730d5
cpsoucy <96217410+cpsoucy@users.noreply.github.com>
2022-12-15 11:20:27 -0500

    attempted to add trigram icon and javascript

--- js/page.js
+++ js/page.js
@@ -1,1 +1,11 @@
-
+function navToggle() {
+    let navbar_nav = document.getElementById('site-nav');
+    let trigram = document.getElementById('trigram');
+    if (navbar_nav.className === 'nav-wrap') {
+      navbar_nav.classList.add('mobile');
+      trigram.classList.add('tri-mobile');
+    } else {
+      navbar_nav.classList = 'nav-wrap';
+      trigram.classList = 'nav-link';
+    }
+  }
